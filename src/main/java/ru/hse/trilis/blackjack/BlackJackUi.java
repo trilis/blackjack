0542c8ed2a4ee50e7ef1c449448f47257b5c0ca1
123a6bcw <ga.karandash@yandex.ru>
2019-09-18 16:42:57 +0300

    Added project!!!!!!!

---
+++ src/main/java/ru/hse/trilis/blackjack/BlackJackUi.java
@@ -0,0 +1,13 @@
+package ru.hse.trilis.blackjack;
+
+import javafx.scene.canvas.GraphicsContext
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Controller of game logic.
+ */
+public class BlackJackUi {
+
+}
