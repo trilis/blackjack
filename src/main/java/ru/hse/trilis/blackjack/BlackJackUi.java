64f71301f57b8e0a750d44db5157476de6395b86
vvselischev <800-99@mail.ru>
2019-11-13 18:01:16 +0300

    refactor

--- src/main/java/ru/hse/trilis/blackjack/BlackJackUi.java
+++ src/main/java/ru/hse/trilis/blackjack/BlackJackUi.java
@@ -53,18 +53,21 @@ public class BlackJackUi extends Application {
     @FXML
     public void finishGame() {
         FinalState finalState = game.getFinalState();
-
-        yourScore.setText(Integer.valueOf(finalState.getResults().get(0).getSum()).toString());
-        crScore.setText(Integer.valueOf(finalState.getCroupierSum()).toString());
-
+        setResultLabels(finalState);
         disableButtons();
+        displayAlertMessage(finalState);
+    }
 
+    private void displayAlertMessage(FinalState finalState) {
         String message = getResultMessage(finalState);
-
         var alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setContentText(message);
-
         alert.showAndWait();
+    }
+
+    private void setResultLabels(FinalState finalState) {
+        yourScore.setText(Integer.valueOf(finalState.getResults().get(0).getSum()).toString());
+        crScore.setText(Integer.valueOf(finalState.getCroupierSum()).toString());
     }
 
     @NotNull
